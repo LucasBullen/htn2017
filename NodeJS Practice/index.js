816ee31836d19e07447441f59fc9c13f67060c72
EC2 Default User <ec2-user@ip-172-31-12-87.ec2.internal>
2017-09-17 13:25:55 +0000

    final fixes

--- NodeJS Practice/index.js
+++ NodeJS Practice/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = require('./router');
-
+const cookieParser = require('cookie-parser')
+//app.use('/api', expressJwt({secret: SECRET}));
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+//app.use(cookieParser())
 const app = express();
 //const port = 808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080;
 const port = 3000;
 app.use(express.static('public'));
 app.use(router);
-
-
+app.use(cookieParser());
+app.use('/api', expressJwt({secret: "sec"}));
 app.listen(port,() =>{
     console.log(`Server running on http://localhost:${port}`);
 });
--- NodeJS Practice/router.js
+++ NodeJS Practice/router.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
+const request = require('request');
+const cookieParser = require('cookie-parser')
 // Helper functions
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
 var watson = require('watson-developer-cloud');
+var schedule = require('node-schedule');
 var visual_recognition = watson.visual_recognition({
   api_key: '9ff8d9a65b96f84e0a4fc610c12b098338997c70',
   version: 'v3',
   version_date: '2016-05-20'
 });
-
+//app.use(cookieParser());
 var fs = require('fs');
-
+var SECRET = 'sec';
 var blockchainInvoke = require('../../fabric-samples/artchain/invoke');
 var blockchainQuery = require('../../fabric-samples/artchain/query');
-
+var coinApiKey = '80abb6aafe606adb604c0103a3c4acb3f30b1e453c25c13166019995c9aaa78a';
+var coinApiSecret = '33474d29164527f162ae8ccb57109b162343bb60850427530110b243a68be737';
 var Client = require('coinbase').Client;
 var client = new Client({'apiKey': '80abb6aafe606adb604c0103a3c4acb3f30b1e453c25c13166019995c9aaa78a', 
                          'apiSecret': '33474d29164527f162ae8ccb57109b162343bb60850427530110b243a68be737'});
@@ -39,13 +44,82 @@ router.get('/art/classify/:imagePath', (req,res) => {
 }); 
 
 router.get('/signIn', (req,res) =>{
-    res.redirect('https://www.coinbase.com/oauth/authorize?client_id=80abb6aafe606adb604c0103a3c4acb3f30b1e453c25c13166019995c9aaa78a&redirect_uri=http%3A%2F%2Flocalhost%3A3000%0D%0A&response_type=code&scope=wallet%3Auser%3Aread');
+    res.redirect('https://www.coinbase.com/oauth/authorize?client_id=80abb6aafe606adb604c0103a3c4acb3f30b1e453c25c13166019995c9aaa78a&redirect_uri=http%3A%2F%2Fec2-52-91-43-150.compute-1.amazonaws.com%3A3000%2Fhome&response_type=code&scope%3Dwallet%3Atransactions%3Asend%26meta%5Bsend_limit_amount%5D%3D1%26meta%5Bsend_limit_currency%5D%3DUSD%26meta%5Bsend_limit_period%5D%3Dday');
 });
 
-router.get('/profile',(req,res) =>{
-    
-})
-router.get('/home', (req,res,next)=>{
+router.get('/profile/:userId',(req,res) =>{
+ var searchTag = req.params.userId;
+        blockchainQuery.query("queryAllArt",function(allArtJSON){
+        if(!allArtJSON){
+            res.status(500).send('Could not find the art you\'re looking for!');
+        } else{
+                searchVideos = [];
+                var searchKeys = ["author"];
+                allArtJSON.forEach(function(element){
+                    searchKeys.forEach(function(key){
+                        if(element["Record"][key].includes(searchTag)){
+                            searchVideos.push(element);
+                       }
+                    })
+
+                });
+
+
+                var imagePath = `./public/img/all.zip`;
+                            var params = {
+                            images_file: fs.createReadStream(imagePath)
+                            };
+                        var classifyJSON = {};
+                        res.send(JSON.stringify(searchVideos, null, 2));
+    }
+});
+
+});
+
+router.get('/home/', (req,res,next)=>{
+	const code = req.query.code;
+	if(code) {
+	console.log(code);
+	request.post(
+	    'https://api.coinbase.com/oauth/token',
+	    { json: { code: code, grant_type: 'authorization_code',client_id: coinApiKey, client_secret:coinApiSecret, redirect_uri:'http://ec2-52-91-43-150.compute-1.amazonaws.com:3000/home' } },
+	    function (error, response, body) {
+	        if (!error && response.statusCode == 200) {
+	            console.log(body)
+		   // var jBody = JSON.parse(body);
+		    var access_token =body['access_token'];
+
+blockchainQuery.query("queryAllArt", function(allArtJSON){
+                allArtJSON = allArtJSON;
+                if (allArtJSON.length > 7){
+                        console.log(allArtJSON.length);
+                        var sliced = allArtJSON.slice(0,7);
+                        res.send('<pre>'+JSON.stringify({Art:sliced,access_token:access_token}, null, 2)+'</pre>');
+                }else{
+                        res.send('<pre>'+JSON.stringify({Art:allArtJSON,access_token:access_token}, null, 2)+'</pre>');//_user));
+                }
+        });
+
+}}	);
+                // Store userID in sessio   
+//        var _user = {};
+ //       client.getCurrentUser(function(err, user) {
+ //              _user = user;
+  //      });
+}else{
+        blockchainQuery.query("queryAllArt", function(allArtJSON){
+                allArtJSON = allArtJSON;
+                if (allArtJSON.length > 7){
+                        console.log(allArtJSON.length);
+                        var sliced = allArtJSON.slice(0,7);
+                        res.send('<pre>'+JSON.stringify({Art:sliced}, null, 2)+'</pre>');
+                }else{
+                        res.send('<pre>'+JSON.stringify({Art:allArtJSON}, null, 2)+'</pre>');//_user));
+                }
+        });
+}});
+
+router.get('/homeOLD', (req,res,next)=>{
     //if(err){
         //console.error(err);
         //res.status(500).send('Something broke!');
@@ -59,7 +133,7 @@ router.get('/home', (req,res,next)=>{
 	console.log('HEY');
         blockchainQuery.query("queryAllArt", function(allArtJSON){
 		console.log('WOO');
-        	allArtJSON = JSON.parse(allArtJSON);
+        	allArtJSON = allArtJSON;
 		if (allArtJSON.length > 7){
 			console.log(allArtJSON.length);
            		var sliced = allArtJSON.slice(0,7);
@@ -79,79 +153,159 @@ router.get('/home', (req,res,next)=>{
     //}
 });
 
-router.get('/art/queryArtById/:artId', (err,req,res)=>{
-    if(err){
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    }
-
-    const artId = req.params.artId;
-
-    artJSON = blockchainQuery.query("queryArtById",[artId]);
-    if(!artJSON){
-        res.status(500).send('Could not find the art you\'re looking for!');
-    }else{
-        res.send(artJSON);
-    }
-
+router.get('/art/:artId', (req,res)=>{
+    	const artId = req.params.artId;
+ 	blockchainQuery.query("queryAllArt",function(allArtJSON){
+                if(!allArtJSON){
+                    res.status(500).send('Could not find the art you\'re looking for!');
+                } else{
+                        allArtJSON.forEach(function(element){
+                                if(element["Key"].includes(artId)){
+					res.send('<pre>'+JSON.stringify(element, null, 2)+'</pre>');
+				}else{
+					res.status(500).send('Could not find the art you\'re looking for!');
+				}
+			});
+		}
+	});
 });
 
-router.post('/art/list/:artId', (err,req,res) =>{
-    if(err){
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    }else{
-        const artId = req.params.name;
+router.post('/art/list/:artId', (req,res) =>{
+        const artId = req.params.artId;
         blockchainInvoke.invoke("list",[artId],function(success){
         if(!success){
             res.status(500).send('Could not find the art you\'re looking for!');
         }else{
              res.send(true);
         }
-}
-    }
 });
-    
+    //}
+});
 
-router.get('/art/queryAllArt', (err,req,res) =>{
-    if(err){
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
+router.post('/art/bid/:artId/:userId/:token/:bcnAmount', (req,res) =>{
+   // if(err){
+   //     console.error(err.stack);
+   //     res.status(500).send('Something broke!');
+   // }else{
+    const artId = req.params.artId;
+	const userId = req.params.userId;
+	const token = req.params.token;
+	const bcnAmount = req.params.bcnAmount;
+
+	blockchainQuery.query("queryAllArt",function(allArtJSON){
+	        if(!allArtJSON){
+	            res.status(500).send('Could not find the art you\'re looking for!');
+	        } else{
+	                var searchKeys = ["userId"];
+	                allArtJSON.forEach(function(element){
+        				if(element["Key"].includes(artId)){
+                            //console.log(element);
+        					const lastUserId = element["Record"]["highest_bidder_id"];
+        					const lastBid = element["Record"]["bcn_value"];
+        					const owner = element["Record"]["owner_id"];
+
+                            var toOwner = parseFloat(bcnAmount);
+                            if(lastBid){
+                                toOwner -=parseFloat(lastBid);
+                            }
+                            console.log(owner);
+                            console.log(lastBid);
+                            console.log(toOwner);
+                            console.log(bcnAmount);
+                            console.log(token);
+                            request.post('https://api.coinbase.com/v2/accounts/BTC/transactions',
+                                { json: { type: "send", to: "1337YoDVaBUz6BnNi3V9MYp3CnwLf8zwhV", amount: 0.000001, currency:"BTC"/*toOwner*/ },
+                                 headers: {
+                                    'Authorization': "Bearer "+token
+                                 } },
+                                function (error, response, body) {
+                                    if (!error && response.statusCode == 200) {
+                                        console.log(body);
+                                    }else{
+                                        console.log(body);
+                                    }
+                            });
+
+                            request.post('https://api.coinbase.com/v2/accounts/BTC/transactions',
+                                { json: { type: "transfer", to: lastUserId, amount: parseFloat(lastBid) },
+                                 headers: {
+                                    'Authorization': "Bearer "+token
+                                 }  },
+                                function (error, response, body) {
+                                    if (!error && response.statusCode == 200) {
+                                        console.log(body);
+                                    }else{
+                                        console.log("2b:"+response.statusCode);
+                                    }
+                            });
+                            return;
+                        }
+                    });
+    		}
+    });
+    blockchainInvoke.invoke("bid",[artId, userId, bcnAmount],function(success){
+    if(!success){
+        res.status(500).send('Could not find the art you\'re looking for!');
     }else{
+         res.send(true);
+    }
+    });
+});
+
+router.get('/public/image/:image', (req,res,next) =>{
+    const image = req.params.image;
+var img = fs.readFileSync('../art/'+image);
+     res.writeHead(200, {'Content-Type': 'image/jpg' });
+     res.end(img, 'binary');
+             res.send();
+});
+
+router.get('/art/queryAllArt', (req,res,next) =>{
+  //  if(err){
+   //     console.error(err.stack);
+   //     res.status(500).send('Something broke!');
+   // }else{
         blockchainQuery.query("queryAllArt",function(allArtJSON){
         if(!allArtJSON){
             res.status(500).send('Could not find the art you\'re looking for!');
         } else{
              res.send(allArtJSON);
         }
-}
-    }
+        });
+    //}
 });
 
-router.post('/art/auctionSetUp/:artId/:startTime/:endTime', (err,req,res) =>{
-    if(err){
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    }else{
+router.post('/art/auctionSetUp/:artID/:startTime/:endTime/:startingBid', (req,res) =>{
+   // if(err){
+    //    console.error(err.stack);
+    //    res.status(500).send('Something broke!');
+  //  }else{
         const artId = req.params.artID;
         const startTime = req.params.startTime;
         const endTime = req.params.endTime;
-
-        blockchainInvoke.invoke("setUpAuction", [artID, startTime, endTime],function(success){
-        if (!success) {
-            res.status(500).send('Could not begin the auction!');
+        const startingBid = req.params.startingBid;
+	var d = new Date(endTime);
+	var time = d.getSeconds()+' '+d.getMinutes()+' '+d.getHours()+' '+d.getDate()+' '+(1+d.getMonth())+ ' *';
+	var j = schedule.scheduleJob(time, function(){
+		//console.log('The answer to life, the universe, and everything!');
+        blockchainInvoke.invoke("endAuction", [artId],function(success){
+        });
+    });
+        blockchainInvoke.invoke("setUpAuction", [artId, startTime+'', endTime+'', startingBid+''],function(success){
+    if (!success) {
+            res.status(500).send('Could not end the auction!');
         } else {
             res.send(true);
         }
-}
-    }
+});
+    //}
 });
 
-router.post('/art/setStatus/:artId/:status', (err,req,res) =>{
-    if(err){
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    }else{
+router.post('/art/setStatus/:artId/:status', (req,res) =>{
+   // if(err){
+    //    console.error(err.stack);
+    //    res.status(500).send('Something broke!');
+   // }else{
         const artId = req.params.artId;
         const status = req.params.status;
 
@@ -161,15 +315,15 @@ router.post('/art/setStatus/:artId/:status', (err,req,res) =>{
         } else {
             res.send(true);
         }
-}
-    }
+});
+    //}
 });
 
-router.post('/art/setPrice/:artId/:price', (err,req,res) =>{
-    if(err){
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    }else{
+router.post('/art/setPrice/:artId/:price', (req,res) =>{
+   // if(err){
+    //    console.error(err.stack);
+    //    res.status(500).send('Something broke!');
+    //}else{
         const artId = req.params.artId;
         const price = req.params.price;
 
@@ -179,54 +333,60 @@ router.post('/art/setPrice/:artId/:price', (err,req,res) =>{
         } else {
             res.send(true);
         }
-}
-    }
+});
+//    }
 });
 
-router.post('/search/:tag', (err,req,res)=>{
-    if(err){
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    }else{
+router.get('/search/:tag', (req,res,next)=>{
         var searchTag = req.params.tag;
-        blockchainInvoke.invoke("queryAllArt",function(allArtJSON){
+        blockchainQuery.query("queryAllArt",function(allArtJSON){
         if(!allArtJSON){
             res.status(500).send('Could not find the art you\'re looking for!');
         } else{
                 searchVideos = [];
+                var gelement= allArtJSON[0];
                 var searchKeys = ["name","author","description"];
                 allArtJSON.forEach(function(element){
+
                     searchKeys.forEach(function(key){
-                        if(element[key].includes(searchTag)){
+                        if(element["Record"][key].includes(searchTag)){
                             searchVideos.push(element);
-                        }else{
-                            var imagePath = `./public/img/${element[id]}`;
+ 	               }
+		    })
+
+
+
+        });
+
+                var imagePath = `./public/img/all.zip`;
                             var params = {
                             images_file: fs.createReadStream(imagePath)
                             };
-
-                            visual_recognition.classify(params, function(err, res2) {
+                        var classifyJSON = {};
+            visual_recognition.classify(params, function(err, res2) {
                                 if (err)
                                     console.log(err);
                                 else{
-                                    classifyJSON = JSON.stringify(res2, null, 2);
-                                }
-                    
-                            });
-
+                                    classifyJSON = res2;//JSON.stringify(res2, null, 2);
+                                //console.log(JSON.stringify(res2, null, 2));
                             var classes = classifyJSON['images'][0]['classifiers'][0]['classes'];
-                            classes.forEach(function(bucket){
-                                if(bucket['class'].includes(searchTag)){
-                                    searchVideos.push(element);
+                            try{
+                classes.forEach(function(bucket){
+                                //console.log(bucket['class'],"and",searchTag);
+                if(bucket['class'].includes(searchTag)){
+                                    searchVideos.push(gelement);
+                                    throw BreakException;
                                 }
                             })
+                        }catch (e){
+                            //tee
                         }
-                    })   
-                });
-            res.send(searchVideos);
-        }
-}
+                         res.send("<pre>"+JSON.stringify(searchVideos, null, 2)+"</pre>");
+                                    return;
+                                }
+
+        });
     }
 });
-
+});
 module.exports = router;
