9eff23c824bb621783c6c6cb6cbe7e75e0b6c22b
Vinith MS <suriyaku@gmail.com>
2017-09-16 21:57:54 -0400

    Added several endpoints and search.

--- NodeJS Practice/router.js
+++ NodeJS Practice/router.js
@@ -11,6 +11,8 @@ var visual_recognition = watson.visual_recognition({
 
 var fs = require('fs');
 
+var blockchainInvoke = require('invoke');
+
 // Routes
 
 router.get('/art/classify/:imagePath', (req,res) => {
@@ -30,17 +32,42 @@ router.get('/art/classify/:imagePath', (req,res) => {
 }); 
 
 router.get('/signIn', (req,res) =>{
-    //var 
-    //res.send('Hello world!');
+    res.redirect('https://www.coinbase.com/oauth/authorize?client_id=80abb6aafe606adb604c0103a3c4acb3f30b1e453c25c13166019995c9aaa78a&redirect_uri=http%3A%2F%2Flocalhost%3A3000%0D%0A&response_type=code&scope=wallet%3Auser%3Aread');
 });
 
-router.get('/art/queryArtByName/:name', (err,req,res)=>{
+router.get('/profile',(req,res) =>{
+    
+})
+
+router.get('/home', (err,req,res)=>{
+    if(err){
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    }else{
+        allArtJSON = blockchainInvoke.invoke("queryAllArt");
+        if (allArtJSON.length > 7){
+            var sliced = allArtJSON.sliced(0,7);
+            res.send(sliced);
+        }else{
+            res.send(allArtJSON);
+        }   
+    }
+});
+
+router.get('/art/queryArtById/:artId', (err,req,res)=>{
     if(err){
         console.error(err.stack);
         res.status(500).send('Something broke!');
     }
 
-    const name = req.params.name;
+    const artId = req.params.artId;
+
+    artJSON = blockchainInvoke.invoke("queryArtById",[artId]);
+    if(!artJSON){
+        res.status(500).send('Could not find the art you\'re looking for!');
+    }else{
+        res.send(artJSON);
+    }
 
 });
 
@@ -50,6 +77,12 @@ router.post('/art/list/:artId', (err,req,res) =>{
         res.status(500).send('Something broke!');
     }else{
         const artId = req.params.name;
+        success = blockchainInvoke.invoke("list",[artId]);
+        if(!success){
+            res.status(500).send('Could not find the art you\'re looking for!');
+        }else{
+             res.send(true);
+        }
     }
 });
     
@@ -59,16 +92,31 @@ router.get('/art/queryAllArt', (err,req,res) =>{
         console.error(err.stack);
         res.status(500).send('Something broke!');
     }else{
+        allArtJSON = blockchainInvoke.invoke("queryAllArt");
+        if(!allArtJSON){
+            res.status(500).send('Could not find the art you\'re looking for!');
+        } else{
+             res.send(allArtJSON);
+        }
 
     }
 });
 
-router.post('/art/purchase/:artId', (err,req,res) =>{
+router.post('/art/auctionSetUp/:artId/:startTime/:endTime', (err,req,res) =>{
     if(err){
         console.error(err.stack);
         res.status(500).send('Something broke!');
     }else{
-        const artId = req.params.name;
+        const artId = req.params.artID;
+        const startTime = req.params.startTime;
+        const endTime = req.params.endTime;
+
+        success = blockchainInvoke.invoke("setUpAuction", [artID, startTime, endTime]);
+        if (!success) {
+            res.status(500).send('Could not begin the auction!');
+        } else {
+            res.send(true);
+        }
     }
 });
 
@@ -77,7 +125,15 @@ router.post('/art/setStatus/:artId/:status', (err,req,res) =>{
         console.error(err.stack);
         res.status(500).send('Something broke!');
     }else{
-        const artId = req.params.name;
+        const artId = req.params.artId;
+        const status = req.params.status;
+
+        success = blockchainInvoke.invoke("setStatus", [artId, status]);
+        if (!success) {
+            res.status(500).send("Could not set status!");
+        } else {
+            res.send(true);
+        }
     }
 });
 
@@ -86,12 +142,62 @@ router.post('/art/setPrice/:artId/:price', (err,req,res) =>{
         console.error(err.stack);
         res.status(500).send('Something broke!');
     }else{
-        const artId = req.params.name;
+        const artId = req.params.artId;
+        const price = req.params.price;
+
+        success = blockchainInvoke.invoke("setPrice", [artId, price]);
+        if (!success) {
+            res.status(500).send("Could not set price!");
+        } else {
+            res.send(true);
+        }
     }
 });
 
-router.get('/art/initLedger',(err,req,res) =>{
+router.post('/search/:tag', (err,req,res)=>{
+    if(err){
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    }else{
+        var searchTag = req.params.tag;
+        allArtJSON = blockchainInvoke.invoke("queryAllArt");
+        if(!allArtJSON){
+            res.status(500).send('Could not find the art you\'re looking for!');
+        } else{
+                searchVideos = [];
+                var searchKeys = ["name","author","description"];
+                allArtJSON.forEach(function(element){
+                    searchKeys.forEach(function(key){
+                        if(element[key].includes(searchTag)){
+                            searchVideos.push(element);
+                        }else{
+                            var imagePath = `./public/img/${element[id]}`;
+                            var params = {
+                            images_file: fs.createReadStream(imagePath)
+                            };
 
+                            visual_recognition.classify(params, function(err, res2) {
+                                if (err)
+                                    console.log(err);
+                                else{
+                                    classifyJSON = JSON.stringify(res2, null, 2);
+                                }
+                    
+                            });
+
+                            var classes = classifyJSON['images'][0]['classifiers'][0]['classes'];
+                            classes.forEach(function(bucket){
+                                if(bucket['class'].includes(searchTag)){
+                                    searchVideos.push(element);
+                                }
+                            })
+                        }
+                    })   
+                });
+            res.send(searchVideos);
+        }
+
+    }
 });
 
 module.exports = router;
